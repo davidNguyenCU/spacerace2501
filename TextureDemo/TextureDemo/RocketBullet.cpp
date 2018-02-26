49fa37adb590d5e0a0d38d60270161887ae77825
David Nguyen <dangtam16@gmail.com>
2018-02-26 16:20:17 -0500

    Reverted

--- TextureDemo/TextureDemo/RocketBullet.cpp
+++ TextureDemo/TextureDemo/RocketBullet.cpp
@@ -4,7 +4,6 @@ RocketBullet::RocketBullet(glm::vec3 &entityPos, glm::vec3 &entityScale, float e
 	: Bullet(entityPos, entityScale, entityRotationAmount, entityTexture, entityNumElements, bulletVelocity)
 {
 }
-//Testing
 
 void RocketBullet::update(double deltaTime) {
 	position += velocity * (float)deltaTime;
